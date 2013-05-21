8d9e3d33a381d2357c05629aaaccb37e9dd8b2a7
joanne <jemmaalcain@gmail.com>
2013-05-21 12:20:58 +0800

    updated project ->checking first the schedule before adding new schedule

---
+++ app/teacher_view_data_admin.php
@@ -0,0 +1,13 @@
+<?php
+      
+      include 'dao/TeacherDAO.php';
+   session_start();
+   	$teacher = $_POST['teacher_id'];
+      $action= new TeacherDAO();
+      
+    
+     $action->teacher_view_profile_via_admin( $teacher);
+     
+     
+   
+?>
